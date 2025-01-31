55107289b1848d1ab364d7d2094b77cb3f7a7d1e
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-31 19:27:23 +0530

    Add files via upload

---
+++ SQL_EP_1.sql
@@ -0,0 +1,12 @@
+-- how to create database:
+create database school;
+create database college;
+
+-- how to delete database:
+drop database school;
+
+-- how to the use the database to create , update , read , delete to the  particular database table:
+use school;
+
+-- how to see the databases:
+show databases;
