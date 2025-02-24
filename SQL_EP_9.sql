e7be73dbe2ca758f3a6c0d6b4e1e04d9a10a1e61
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-02-24 18:55:43 +0530

    Add files via upload

---
+++ SQL_EP_9.sql
@@ -0,0 +1,45 @@
+-- in this chapter we will learn about Where Clause commands and its works:
+
+create database company;
+
+use company;
+
+create table employee(
+id int primary key,
+name varchar(50),
+age int ,
+department varchar(50),
+city varchar(50),
+salary int
+);
+
+insert into employee(id,name,age,department,city,salary)
+values
+(1,"rahul",21,"IT","Mumbai",1500),
+(2,"afsara",26,"hr","pune",2000),
+(3,"abi",27,"IT","Mumbai",2500),
+(4,"raj",25,"marketing","surat",2400),
+(5,"adhi",24,"finace","indore",2000);
+
+select * from employee;
+
+SET SQL_SAFE_UPDATES = 0; -- use this query while you have got the error like safe mode
+update employee
+set salary = 50000
+where department = "hr";
+
+-- delete command:
+
+ -- delete from  employee
+-- where department = "IT";
+
+-- select command:
+
+-- select name,age from employee;
+-- select * from employee;
+
+
+-- where clause command:
+
+select * from employee
+where age>25;
